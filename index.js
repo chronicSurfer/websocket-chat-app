2caeabbcc3aded7a3916e45e9b44c42ab3f94520
chronicSurfer <kyle.umbarger@gmail.com>
2019-02-15 16:18:04 -0800

    Created basic server setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+//App setup
+const app = express();
+
+//Server setup
+const server = app.listen(5000, function(){
+    console.log("The server is listening to your thoughts!");
+})
