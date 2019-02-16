22f1c605b979f1bbda07cb40afb41eb28bce1799
chronicSurfer <kyle.umbarger@gmail.com>
2019-02-15 16:43:36 -0800

    Created Static Files

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ const app = express();
 const server = app.listen(5000, function(){
     console.log("The server is listening to your thoughts!");
 })
+
+//static file
+app.use(express.static('public'));
